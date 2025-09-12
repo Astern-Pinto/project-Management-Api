fa083eb6203752340c3cab88ebfcaadb80dc0292
Astern <asternp26@gmail.com>
2025-09-12 16:05:22 +0530

    add standard api error response

---
+++ src/utils/api-error.js
@@ -0,0 +1,25 @@
+class ApiError extends Error{
+    constructor(
+        statusCode,
+        error=[],
+        message="something went wronge",
+        stack = ""
+         ) {
+
+        super(message)
+        this.statusCode = statusCode
+        this.error = error
+        this.message = message
+        this.data = null
+        this.success = false
+
+        if(stack){
+            this.stack = stack
+        }else{
+            Error.captureStackTrace(this, this.constructor)
+        }
+        
+    }
+}
+
+export {ApiError};
