ed3b30c93666ee71376fe5ed237a9e63fbe9c451
Astern <asternp26@gmail.com>
2025-09-12 16:15:24 +0530

    restructed app file

---
+++ src/app/app.js
@@ -0,0 +1,28 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(express.json("16kb"));
+app.use(express.urlencoded({extended:true,limit:"16kb"}));
+app.use(express.static("public"));
+
+
+//CORS configuration
+app.use(cors({
+    origin:process.env.CORS_ORIGIN ?.split(",") || "http://localhost:5173",
+    credentials:true,
+    methods:["GET","POST","PUT","DELETE","PATCH","OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+}),)
+
+//Importing Routes
+
+import healthCheckRoute from "../routes/healthcheck.route.js";
+
+app.use("/api/v1/healthcheck",healthCheckRoute);
+
+
+
+
+export default app
